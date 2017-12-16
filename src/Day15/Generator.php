9545c8ef7933c9c3cfcc9e4836a7a73d19d33166
Hauke Stange <partyschaum@posteo.de>
2017-12-16 18:24:51 +0100

    Remove unnecessary check for zero

--- src/Day15/Generator.php
+++ src/Day15/Generator.php
@@ -5,9 +5,6 @@ namespace Partyschaum\AdventOfCode\Day15;
 class Generator
 {
     /** @var int */
-    private $seed;
-
-    /** @var int */
     private $factor;
 
     /** @var int */
@@ -18,17 +15,13 @@ class Generator
 
     public function __construct(int $seed, int $factor, int $divisor)
     {
-        $this->seed = $seed;
         $this->factor = $factor;
-        $this->next = 0;
+        $this->next = $seed;
         $this->divisor = $divisor;
     }
 
     public function generate(): int
     {
-        if ($this->next === 0) {
-            $this->next = $this->seed;
-        }
         $this->next = $this->next * $this->factor % $this->divisor;
         return $this->next;
     }
